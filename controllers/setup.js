b7c38a196b197808f297cb0b30f14d402e946636
kushal-kamra <kushalkamradev@gmail.com>
2023-02-27 11:06:36 +0530

    Add transaction in setup

--- controllers/setup.js
+++ controllers/setup.js
@@ -1,8 +1,16 @@
-const Wallet = require('../models/wallet');
+const mongoose = require('mongoose');
 const uuid = require('uuid');
 
+const Wallet = require('../models/wallet');
+const Transaction = require('../models/transaction');
+
 exports.walletSetup = async (req, res, next) => {
+
+    const session = await mongoose.startSession();
+
     try {
+        await session.startTransaction();
+
         const { name, balance } = req.body;
 
         const wallet = new Wallet({
@@ -10,25 +18,50 @@ exports.walletSetup = async (req, res, next) => {
             balance: Number(balance),
         });
     
-        const result = await wallet.save();
+        const result = await wallet.save({ session });
 
         if (result !== null) {
-            return res.status(200).json({
-                id: result["_id"],
-                balance: result["balance"].toString(),
-                name: result["name"],
-                date: result["date"],
-            });
+            const transaction = new Transaction(
+                {
+                    walletId: result["_id"],
+                    amount: Number(balance),
+                    balance: Number(balance),
+                    description: "Setup",
+                    type: "CREDIT",
+                },
+                {
+                    session: session,
+                }
+            );
+
+            const transaction_result = await transaction.save({ session });
+
+            if (transaction_result !== null) {
+                await session.commitTransaction();
+
+                return res.status(200).json({
+                    id: result["_id"],
+                    balance: result["balance"].toString(),
+                    name: result["name"],
+                    date: result["date"],
+                });
+            }
         }
+
+        await session.abortTransaction();
 
         return res.status(500).json({
             success: false,
             message: "Unable to create a wallet"
         });
     } catch(err) {
+        await session.abortTransaction();
+
         return res.status(500).json({
             success: false,
             message: "Unable to create a wallet"
         });
+    } finally {
+        await session.endSession();
     }
 }
