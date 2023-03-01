4ec5efefad3b381585da45c3a9b3347711ed3a07
kushal-kamra <kushalkamradev@gmail.com>
2023-03-01 16:01:46 +0530

    enable cors for all

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
